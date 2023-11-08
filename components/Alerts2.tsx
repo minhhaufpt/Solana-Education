7f16970d48b87efa89ea1c768a3b1c5b251a09c5
Uber333 <129860903+Uber333@users.noreply.github.com>
2023-11-08 20:27:47 +0700

    Create Alerts2.tsx

---
+++ components/Alerts2.tsx
@@ -0,0 +1,22 @@
+import {type} from 'os';
+import Swal from 'sweetalert2';
+type info = {
+  txt: string;
+  time: number;
+};
+const Alerts2 = ({txt, time}:info) => {
+  const toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: time,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
+  return <></>;
+};
+export default Alerts2;
+
